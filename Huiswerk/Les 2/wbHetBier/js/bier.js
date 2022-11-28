31c995375abd7b11ab784e93816f36c61ccc9f6c
InsaWitterland <102620148+InsaWitterland@users.noreply.github.com>
2022-11-28 23:54:06 +0100

    ik loop vast

---
+++ Huiswerk/Les 2/wbHetBier/js/bier.js
@@ -0,0 +1,7 @@
+console.log("test")
+
+var namen = ["Anouk", "Priscilla", "Enrique", "Lizzy"]
+
+var pElement = document.querySelector(p);
+
+pElement.textContent = "ik snap het niet, hoe werkt deze functie"; 
